b68ef147ffd689c147830b766b59e0c2c9b611e3
Neha Kumari <chandravanshi.neha01@gmail.com>
2022-12-18 22:48:17 +0530

    Add TopBanner Image

---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Fragment } from 'react'
+import { Container } from "react-bootstrap"
+import "../../assets/css/custom.css"
+
+class TopBanner extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container className="topFixedBanner"></Container>
+
+      </Fragment>
+    )
+  }
+}
+
+export default TopBanner
