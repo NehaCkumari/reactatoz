57cb21cd1b709f342d6e1eac4be0398188573ff5
Neha Kumari <chandravanshi.neha01@gmail.com>
2022-12-19 12:30:30 +0530

    Add images

--- src/components/Services/services.jsx
+++ src/components/Services/services.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
 import { Component, Fragment } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
+import DesignIcon from "../../assets/images/design.png";
+import EcommerceIcon from "../../assets/images/ecommerce.png";
+import WebIcon from "../../assets/images/web.png";
 
 class services extends Component {
   render() {
     return (
-        <Fragment>
-            <Container>
-                <Row>
-                    <Col lg={4} md={6} sm={12} >
-                    <h1>One</h1>
-                    </Col>
-                    <Col lg={4} md={6} sm={12} >
-                    <h1>Two</h1>
-                    </Col>
-                    <Col lg={4} md={6} sm={12} >
-                    <h1>Three</h1>
-                    </Col>
-                </Row>
-            </Container>
-        </Fragment>
+<Fragment>
+    <Container>
+        <Row>
+            <Col lg={4} md={6} sm={12} >
+                <div>
+                    <img src="DesignIcon" />
+                </div>
+            </Col>
+            <Col lg={4} md={6} sm={12} >
+                <div>
+                    <img src="EcommerceIcon" />
+                </div>
+            </Col>
+            <Col lg={4} md={6} sm={12} >
+                <div>
+                    <img src="WebIcon" />
+                </div>
+            </Col>
+        </Row>
+    </Container>
+</Fragment>
 
     )
   }
